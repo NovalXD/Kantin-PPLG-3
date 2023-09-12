c4446b95a2cd288d578f7d7f471287b0be12597a
NovalXD <140633218+NovalXD@users.noreply.github.com>
2023-09-12 21:24:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -91,10 +91,12 @@ products.forEach(e => {
                 <div class="product-card-info">
                     <div class="product-btn">
                     <a href="products.html" class="btn-flat btn-hover btn-shop-now">shop now</a>
+                        <a href="products.html</a>
                         <button class="btn-flat btn-hover btn-cart-add">
                             <i class='bx bxs-cart-add'></i>
                         </button>
                         <button class="btn-flat btn-hover btn-cart-add">
+                            <a href="products.html</a>
                             <i class='bx bxs-heart'></i>
                         </button>
                     </div>
